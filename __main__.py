9437292d2fd4f8f314c301d210f9ff6ebf29080f
akk0ga <45255672+akk0ga@users.noreply.github.com>
2021-03-25 00:22:59 +0100

    v1 image to ascii

--- __main__.py
+++ __main__.py
@@ -3,12 +3,12 @@ from image.Transform import Transform
 
 class App:
     def __init__(self):
-        self.__transform = Transform(image_path='img/original/smiley.jpg')
+        self.__transform = Transform(image_path='img/original/test.jpg')
 
     def run(self):
-        image_name = 'smiley'
-        self.__transform.to_greyscale(image_name=image_name)
-        # self.__transform.to_ascii(new_image_name=image_name)
+        image_name = 'greyscale'
+        self.__transform.to_greyscale(image_name=image_name, new_width=2272, new_height=1704)
+        self.__transform.to_ascii(image_name=image_name, image_path='img/greyscale')
 
 
 if __name__ == "__main__":
--- image/Transform.py
+++ image/Transform.py
@@ -14,14 +14,20 @@ class Transform(Image):
         self.image = image_path
         self.__ascii_list = '@*/.,%$()&+-^_:<>=;!?'
 
-    def to_greyscale(self, image_name: str, path: str = 'img/greyscale') -> None:
+    def to_greyscale(self, new_height: int, new_width: int, image_name: str, path: str = 'img/greyscale') -> None:
         """
         resize convert and save RGB image to resize grayscale image
+        :param new_width:
+        :param new_height:
         :param path:
         :param image_name:
         :return:
         """
-        resize = self.image.resize((800, 500))
+        # keep aspect ratio
+        width = int(new_height / new_height * new_width)
+
+        # resize and save
+        resize = self.image.resize((width, new_height))
         img_greyscale = resize.convert('L')
         img_greyscale.save(f'{path}/{image_name}.png')
 
@@ -42,36 +48,43 @@ class Transform(Image):
                     pixel_color[pixel] = self.__ascii_list[randint(0, len(self.__ascii_list) - 1)]
         return pixel_color
 
-    def to_ascii(self, new_image_name: str = 'new', save_path: str = 'img/ascii',
-                 greyscale_path: str = 'img/greyscale') -> None:
+    def to_ascii(self, image_name: str, image_path: str) -> None:
         """
+        use Courier font
         create and save new image in ascii art
         :return:
         """
         # load and get greyscale image and info
-        self.image = f'{greyscale_path}/{new_image_name}.png'
+        self.image = f'{image_path}/{image_name}.png'
         image_size: tuple = self._get_info()['size']
+        print(image_size)
 
-        # get pixel color
-        pixel_color = self.get_pixel_color(image_size=image_size)
+        # ascii list
+        ascii = {
+            '0-29': ' ',
+            '30-50': '+',
+            '51-80': ':',
+            '81-110': '/',
+            '111-140': '-',
+            '141-170': '=',
+            '171-200': '@',
+            '201-230': '$',
+            '231-255': '!'
+        }
 
-        # create new empty image
-        print(pixel_color)
-
-        # load the image create before to draw on
-        new_img = PilImg.open(f'{save_path}/{new_image_name}.png')
-        set_font_size = PilFont.truetype('arial.ttf', 1)
-        add_ascii = PilDraw.Draw(new_img)
-
-        for y in range(0, image_size[1]):
-            for x in range(0, image_size[0]):
-                coordinate: tuple = (x, y)
-                pixel = self.image.getpixel(xy=coordinate)
-
-                # add character
-                if pixel in pixel_color and pixel > 10:
-                    add_ascii.text(coordinate, pixel_color[pixel], 1000, font=set_font_size)
-                    print(f'{x},{y} -> {pixel_color[pixel]}')
-
-        new_img.save(f'{save_path}/{new_image_name}.png')
-        new_img.close()
+        # open text file
+        with open('draw.txt', 'w') as data:
+            for y in range(0, image_size[1]):
+                for x in range(0, image_size[0]):
+                    coordinate = (x, y)
+                    color = self.image.getpixel(coordinate)
+                    for key in ascii:
+                        new_key = key.replace('-', ' ')
+                        val_min, val_max = new_key.split()
+                        if int(val_min) <= color <= int(val_max):
+                            if x != image_size[0]-1:
+                                data.write(ascii[f'{val_min}-{val_max}'])
+                            else:
+                                char = ascii[f'{val_min}-{val_max}']
+                                data.write(f'{char}\n')
+            data.close()
