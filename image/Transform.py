f796dade5981e6e3146004a32e75df6dc6e88f5c
akk0ga <45255672+akk0ga@users.noreply.github.com>
2021-03-24 01:11:58 +0100

    refactor to_ascii in Transform

--- image/Transform.py
+++ image/Transform.py
@@ -26,17 +26,19 @@ class Transform(Image):
         img_greyscale = img_load.convert('L')
         img_greyscale.save(f'{path}/{image_name}.png')
 
-    def to_ascii(self) -> None:
+    def to_ascii(self, image_name: str = 'new', path: str = 'img/ascii') -> None:
         """
         create and save new image in ascii art
         :return:
         """
+        # get image size
         size = self._get_info()[1]
+        # create empty dict to stock pixel value later
         pixel_color: dict = {}
 
         # create new image and save it
         new_img = PilImg.new(mode='L', size=size)
-        new_img.save('img/new.png')
+        new_img.save(f'{path}/{image_name}.png')
         new_img.close()
 
         # get all shade of gray from the original image
@@ -58,9 +60,10 @@ class Transform(Image):
         time.sleep(3)
 
         # check the pixel color to add the correct character
-        new_img = PilImg.open('img/new.png')
+        new_img = PilImg.open(f'{path}/{image_name}.png')
         set_font_size = PilFont.truetype('arial.ttf', 1)
         add_ascii = PilDraw.Draw(new_img)
+
         for y in range(0, size[1]):
             for x in range(0, size[0]):
                 coordinate: tuple = (x, y)
