ec0e25b7ffde9f14b75066166cd074b9e3785729
akk0ga <45255672+akk0ga@users.noreply.github.com>
2021-03-24 17:21:31 +0100

    add param in to_ascii
    make path for image and name dynamic

--- __main__.py
+++ __main__.py
@@ -4,10 +4,12 @@ from image.Image import Image
 
 class App:
     def __init__(self):
-        self.__transform = Transform(image_path='img/original/toga.jpg')
+        self.__transform = Transform(image_path='img/original/smiley.jpg')
 
     def run(self):
-        self.__transform.to_ascii(new_image_name='toga')
+        image_name = 'smiley'
+        self.__transform.to_greyscale(image_name=image_name)
+        self.__transform.to_ascii(new_image_name=image_name)
 
 
 if __name__ == "__main__":
--- image/Transform.py
+++ image/Transform.py
@@ -26,51 +26,49 @@ class Transform(Image):
         img_greyscale = img_load.convert('L')
         img_greyscale.save(f'{path}/{image_name}.png')
 
-    def to_ascii(self, new_image_name: str = 'new', path: str = 'img/ascii') -> None:
+    def to_ascii(self, new_image_name: str = 'new', save_path: str = 'img/ascii', greyscale_path: str = 'img/greyscale') -> None:
         """
         create and save new image in ascii art
         :return:
         """
-        # get image size
-        size = self._get_info()['size']
-        # create empty dict to stock pixel value later
+        image_size = self._get_info()['size']
         pixel_color: dict = {}
 
         # replace the original with the greyscale image
-        self.image = f'img/greyscale/{new_image_name}.png'
+        self.image = f'{greyscale_path}/{new_image_name}.png'
 
         # create new empty image and save it
-        new_img = PilImg.new(mode='L', size=size)
-        new_img.save(f'{path}/{new_image_name}.png')
+        new_img = PilImg.new(mode='L', size=image_size)
+        new_img.save(f'{save_path}/{new_image_name}.png')
         new_img.close()
 
         # check the shade of grey for each pixel
-        for y in range(0, size[1]):
-            for x in range(0, size[0]):
+        for y in range(0, image_size[1]):
+            for x in range(0, image_size[0]):
                 coordinate: tuple = (x, y)
                 pixel = self.image.getpixel(xy=coordinate)
 
                 # stock pixel in the color list and assign ascii to it
-                if pixel not in pixel_color and pixel != 0:
+                if pixel not in pixel_color and pixel > 10:
                     pixel_color[pixel] = self.__ascii_list[randint(0, len(self.__ascii_list) - 1)]
                     print(f'({x}, {y}) -> {pixel}')
         print('CHAR ATTRIBUTION END')
         time.sleep(3)
 
         # check the pixel color to add the correct character
-        new_img = PilImg.open(f'{path}/{new_image_name}.png')
+        new_img = PilImg.open(f'{save_path}/{new_image_name}.png')
         set_font_size = PilFont.truetype('arial.ttf', 1)
         add_ascii = PilDraw.Draw(new_img)
 
-        for y in range(0, size[1]):
-            for x in range(0, size[0]):
+        for y in range(0, image_size[1]):
+            for x in range(0, image_size[0]):
                 coordinate: tuple = (x, y)
                 pixel = self.image.getpixel(xy=coordinate)
 
                 # add character
-                if pixel in pixel_color and pixel != 0:
+                if pixel in pixel_color and pixel > 10:
                     add_ascii.text(coordinate, pixel_color[pixel], 1000, font=set_font_size)
                     print(f'{x},{y} -> {pixel_color[pixel]}')
 
-        new_img.save(f'{path}/{new_image_name}.png')
+        new_img.save(f'{save_path}/{new_image_name}.png')
         new_img.close()
