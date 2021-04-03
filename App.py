40d767b314e61f4f0cd35dd5a69c8d5d6a7013c0
akk0ga <45255672+akk0ga@users.noreply.github.com>
2021-04-03 15:36:09 +0200

    reformat file

--- App.py
+++ App.py
@@ -8,6 +8,7 @@ class App:
     def run(self):
         self.__transform.to_ascii(new_width=500, new_height=200, rate_color=13)
 
+
 if __name__ == "__main__":
     app = App(image_path='img/original/mario.jpg')
     app.run()
