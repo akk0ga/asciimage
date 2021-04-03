808845e9718381f017e2a8180ce82f8fa129a242
akk0ga <45255672+akk0ga@users.noreply.github.com>
2021-04-03 15:35:44 +0200

    update App

--- App.py
+++ App.py
@@ -7,8 +7,6 @@ class App:
 
     def run(self):
         self.__transform.to_ascii(new_width=500, new_height=200, rate_color=13)
-        # self.__transform.create_char_dict()
-
 
 if __name__ == "__main__":
     app = App(image_path='img/original/mario.jpg')
