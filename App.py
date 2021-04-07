8852f32b2c48328d85d5b484ec0868bbb2da8b55
akk0ga <45255672+akk0ga@users.noreply.github.com>
2021-04-08 01:40:40 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,9 +1,9 @@
-from transform.Transform import Transform
+from transform.TransformImage import TransformImage
 
 
 class App:
     def __init__(self, image_path: str):
-        self.__transform = Transform(image_path=image_path)
+        self.__transform = TransformImage(image_path=image_path)
 
     def run(self):
         self.__transform.to_ascii(new_size=(500, 200))
