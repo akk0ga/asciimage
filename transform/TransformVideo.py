9f11b9dd2bf515cf0c6c47a595da04993dd6ef46
akk0ga <45255672+akk0ga@users.noreply.github.com>
2021-04-15 17:17:08 +0200

    create loop to read each pixel from cell

--- transform/TransformVideo.py
+++ transform/TransformVideo.py
@@ -28,7 +28,7 @@ class TransformVideo:
             grid = self.calc_grid(cols=20, rows=20, video_shape=video_grayscale.shape)
             self.draw_grid(video=video_grayscale, grid=grid)
 
-            cv.imshow('video', video_grayscale)
+            cv.imshow('You are so pretty', video_grayscale)
             if cv.waitKey(1) == ord('q'):
                 break
 
@@ -36,10 +36,17 @@ class TransformVideo:
         cv.destroyAllWindows()
 
     def calc_grid(self, cols: int, rows: int, video_shape: tuple) -> tuple:
+        """
+        calc the grid cols and rows
+        :param cols:
+        :param rows:
+        :param video_shape:
+        :return:
+        """
         height, width = video_shape
-        total_cols = width // cols
-        total_rows = height // rows
-        return total_cols, total_rows, height, width
+        cell_cols = width // cols
+        cell_rows = height // rows
+        return cell_cols, cell_rows, height, width
 
     def draw_grid(self, grid: tuple, video):
         """
@@ -48,25 +55,41 @@ class TransformVideo:
         :param video:
         :return:
         """
-        mean: list = []
-        total_cols, total_rows, height, width = grid
+        pixels: list = []
+        total_cols: int = 20
+        total_rows: int = 20
+        i = 1
 
-        for col in range(total_cols, width, total_cols):
+        cell_cols, cell_rows, height, width = grid
+
+        for col in range(cell_cols, width, cell_cols):
             cv.line(video, (col, 0), (col, height), (255, 0, 0), 1)
 
-        for row in range(total_rows, height, total_rows):
+        for row in range(cell_rows, height, cell_rows):
             cv.line(video, (0, row), (width, row), (255, 0, 0), 1)
 
-        for col in range(total_cols, total_cols+total_cols):
-            for row in range(0, total_rows):
-                mean.append(video[row, col])
-                # uncomment to check if take only one cell
-                cv.line(video, (col, row), (col, row), (255, 0, 0), 1)
+        while i <= total_cols:
+            j = 1
+            while j <= total_rows:
+                print(i)
+                for col in range(cell_cols * i-1, cell_cols * i):
+                    for row in range(cell_rows * j-1, cell_rows * j):
+                        pixels.append(video[row, col])
+                        # uncomment to check if take only one cell
+                        # cv.line(video, (col, row), (col, row), (255, 0, 0), 1)
+                j += 1
+            i += 1
 
-        # calc mean
+        self.calc_pixel_mean(pixels=pixels)
+
+    def calc_pixel_mean(self, pixels: list):
+        """
+        calc pixel mean
+        :return:
+        """
         total = 0
-        for value in mean:
-            total = total+value
+        for pixel in pixels:
+            total = total + pixel
 
-        total = total//len(mean)
+        total = total // len(pixels)
         print(total)
